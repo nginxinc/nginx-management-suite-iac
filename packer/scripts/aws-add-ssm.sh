644b1aeb6a4b7b645e32fae40082b4e55023b1a5
Daniel O'Connor <da.oconnor@f5.com>
2023-07-20 15:19:40 +0100

    Only run if redhat

--- packer/scripts/aws-add-ssm.sh
+++ packer/scripts/aws-add-ssm.sh
@@ -6,5 +6,10 @@
 # LICENSE file in the root directory of this source tree.
 set -exo pipefail
 
-sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
-sudo systemctl enable amazon-ssm-agent
+
+source /etc/os-release
+
+if [ "${ID_LIKE}" != "debian" ]; then
+    sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+    sudo systemctl enable amazon-ssm-agent
+fi
